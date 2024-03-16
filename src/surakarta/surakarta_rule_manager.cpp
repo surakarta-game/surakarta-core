fa04a9f0ccced5ed78563c5722167e5ad1f66837
Nictheboy <nictheboy@outlook.com>
2024-03-16 19:23:19 +0800

    feat(rule_manager): implement JudgeMove()

--- src/surakarta/surakarta_rule_manager.cpp
+++ src/surakarta/surakarta_rule_manager.cpp
@@ -1,6 +1,8 @@
 #include "surakarta_rule_manager.h"
 #include <iostream>
-//#include "surakarta_utils.h"
+#include "surakarta_utils.h"
+
+// #define DEBUG_JUDGE_MOVE // Enable this macro to print debug information when judging move.
 
 void SurakartaRuleManager::OnUpdateBoard() {
     // TODO:
@@ -10,29 +12,104 @@ void SurakartaRuleManager::OnUpdateBoard() {
 }
 
 SurakartaIllegalMoveReason SurakartaRuleManager::JudgeMove(const SurakartaMove& move) {
-    // TODO: Implement this function.
+    if (board_->IsInside(move.from) == false || board_->IsInside(move.to) == false)
+        return SurakartaIllegalMoveReason::OUT_OF_BOARD;
 
-    // An example of how to get the color of a piece and the current player.
-    for (unsigned int i = 0; i < board_size_; i++) {
-        for (unsigned int j = 0; j < board_size_; j++) {
-            PieceColor color = (*board_)[i][j]->GetColor();
-            if (color == PieceColor::BLACK) {
-                // i,j is the position of a black piece
-            } else if (color == PieceColor::WHITE) {
-                // i,j is the position of a white piece
-            } else {
-                // i,j is an empty position
+    const auto piece_from = (*board_)[move.from.x][move.from.y];
+    const auto color_from = piece_from->GetColor();
+    const auto curr_player = game_info_->current_player_;
+    const auto oppo_player = ReverseColor(curr_player);
+    if (color_from == PieceColor::NONE)
+        return SurakartaIllegalMoveReason::NOT_PIECE;
+    if (color_from != curr_player)
+        return SurakartaIllegalMoveReason::NOT_PLAYER_PIECE;
+
+    if (move.from.x == move.to.x && move.from.y == move.to.y)
+        return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+
+    const auto piece_to = (*board_)[move.to.x][move.to.y];
+    const auto piece_to_color = piece_to->GetColor();
+    if (piece_to_color != oppo_player && piece_to_color != PieceColor::NONE) {
+        return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+    }
+    if (piece_to->GetColor() == PieceColor::NONE) {  // non-capture
+        if (move.from.x == move.to.x) {
+            if (move.from.y < move.to.y) {
+                for (unsigned int y = move.from.y + 1; y < move.to.y; y++) {
+                    if ((*board_)[move.from.x][y]->GetColor() != PieceColor::NONE)
+                        return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+                }
+                return SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE;
+            } else if (move.from.y > move.to.y) {
+                for (unsigned int y = move.from.y - 1; y > move.to.y; y--) {
+                    if ((*board_)[move.from.x][y]->GetColor() != PieceColor::NONE)
+                        return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+                }
+                return SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE;
+            } else
+                return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+        } else if (move.from.y == move.to.y) {
+            if (move.from.x < move.to.x) {
+                for (unsigned int x = move.from.x + 1; x < move.to.x; x++) {
+                    if ((*board_)[x][move.from.y]->GetColor() != PieceColor::NONE)
+                        return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+                }
+                return SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE;
+            } else if (move.from.x > move.to.x) {
+                for (unsigned int x = move.from.x - 1; x > move.to.x; x--) {
+                    if ((*board_)[x][move.from.y]->GetColor() != PieceColor::NONE)
+                        return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+                }
+                return SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE;
+            } else
+                return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+        } else {
+            return SurakartaIllegalMoveReason::ILLIGAL_NON_CAPTURE_MOVE;
+        }
+    } else {  // capture
+        const SurakartaDirection directions[] = {
+            SurakartaDirection::UP,
+            SurakartaDirection::DOWN,
+            SurakartaDirection::LEFT,
+            SurakartaDirection::RIGHT,
+        };
+        for (const auto start_direction : directions) {
+            auto curr_position = piece_from->GetPosition();
+            auto curr_direction = start_direction;
+            auto util = SurakartaPieceMoveUtil(board_size_);
+            int passed_corner_cnt = 0;
+            while (true) {
+#ifdef DEBUG_JUDGE_MOVE
+                std::cout << "(" << curr_position << ", " << curr_direction << ")" << std::endl;
+#endif
+                const auto next_pair_opt = util.Next(curr_position, curr_direction);
+                if (next_pair_opt.has_value() == false)
+                    break;
+                if (next_pair_opt.value().second != curr_direction)
+                    passed_corner_cnt++;
+                curr_position = next_pair_opt.value().first;
+                curr_direction = next_pair_opt.value().second;
+                if (curr_position == move.to) {
+                    if (passed_corner_cnt > 0) {
+#ifdef DEBUG_JUDGE_MOVE
+                        std::cout << "RET" << std::endl;
+#endif
+                        return SurakartaIllegalMoveReason::LEGAL_CAPTURE_MOVE;
+                    } else {
+                        break;
+                    }
+                }
+                if (curr_position != move.from && (*board_)[curr_position.x][curr_position.y]->GetColor() != PieceColor::NONE)
+                    break;
+                if (curr_position == move.from && passed_corner_cnt == 4)
+                    break;
             }
         }
+#ifdef DEBUG_JUDGE_MOVE
+        std::cout << "RET" << std::endl;
+#endif
+        return SurakartaIllegalMoveReason::ILLIGAL_CAPTURE_MOVE;
     }
-    SurakartaPlayer current_player = game_info_->current_player_;
-    if (current_player == SurakartaPlayer::BLACK) {
-        // black player's turn
-    } else if (current_player == SurakartaPlayer::WHITE) {
-        // white player's turn
-    }
-
-    return SurakartaIllegalMoveReason::LEGAL;
 }
 
 std::pair<SurakartaEndReason, SurakartaPlayer> SurakartaRuleManager::JudgeEnd(const SurakartaIllegalMoveReason reason) {
--- src/surakarta/surakarta_utils.h
+++ src/surakarta/surakarta_utils.h
@@ -16,7 +16,7 @@ class SurakartaPieceMoveUtil {
     SurakartaPieceMoveUtil(const int board_size)
         : board_size_(board_size){};
 
-    std::optional<SurakartaPosition> Next(const SurakartaPosition position, const SurakartaDirection direction) {
+    std::optional<std::pair<SurakartaPosition, SurakartaDirection>> Next(const SurakartaPosition position, const SurakartaDirection direction) {
         const std::pair<int, int> next =
             direction == SurakartaDirection::UP
                 ? std::pair(position.x, position.y - 1)
@@ -29,33 +29,51 @@ class SurakartaPieceMoveUtil {
                 : throw new std::logic_error("Invalid direction");
         // Inside:
         if (next.first >= 0 && next.first < board_size_ && next.second >= 0 && next.second < board_size_)
-            return SurakartaPosition(next.first, next.second);
+            return std::pair(SurakartaPosition(next.first, next.second), direction);
 
         // Upper:
         if (next.first >= 1 && next.first < board_size_ / 2 && next.second == -1)
-            return SurakartaPosition(0, next.first);
+            return std::pair(SurakartaPosition(0, next.first), SurakartaDirection::RIGHT);
         if (next.first >= board_size_ / 2 && next.first < board_size_ - 1 && next.second == -1)
-            return SurakartaPosition(board_size_ - 1, board_size_ - next.first - 1);
+            return std::pair(SurakartaPosition(board_size_ - 1, board_size_ - next.first - 1), SurakartaDirection::LEFT);
 
         // Lower:
         if (next.first >= 1 && next.first < board_size_ / 2 && next.second == board_size_)
-            return SurakartaPosition(0, board_size_ - next.first - 1);
+            return std::pair(SurakartaPosition(0, board_size_ - next.first - 1), SurakartaDirection::RIGHT);
         if (next.first >= board_size_ / 2 && next.first < board_size_ - 1 && next.second == board_size_)
-            return SurakartaPosition(board_size_ - 1, next.first);
+            return std::pair(SurakartaPosition(board_size_ - 1, next.first), SurakartaDirection::LEFT);
 
         // Left:
         if (next.first == -1 && next.second >= 1 && next.second < board_size_ / 2)
-            return SurakartaPosition(next.second, 0);
+            return std::pair(SurakartaPosition(next.second, 0), SurakartaDirection::DOWN);
         if (next.first == -1 && next.second >= board_size_ / 2 && next.second < board_size_ - 1)
-            return SurakartaPosition(board_size_ - next.second - 1, board_size_ - 1);
+            return std::pair(SurakartaPosition(board_size_ - next.second - 1, board_size_ - 1), SurakartaDirection::UP);
 
         // Right:
         if (next.first == board_size_ && next.second >= 1 && next.second < board_size_ / 2)
-            return SurakartaPosition(board_size_ - next.second - 1, 0);
+            return std::pair(SurakartaPosition(board_size_ - next.second - 1, 0), SurakartaDirection::DOWN);
         if (next.first == board_size_ && next.second >= board_size_ / 2 && next.second < board_size_ - 1)
-            return SurakartaPosition(next.second, board_size_ - 1);
+            return std::pair(SurakartaPosition(next.second, board_size_ - 1), SurakartaDirection::UP);
 
         // Corner:
         return std::nullopt;
     }
 };
+
+inline std::ostream& operator<<(std::ostream& os, const SurakartaDirection& dir) {
+    switch (dir) {
+        case SurakartaDirection::UP:
+            os << "UP";
+            break;
+        case SurakartaDirection::DOWN:
+            os << "DOWN";
+            break;
+        case SurakartaDirection::LEFT:
+            os << "LEFT";
+            break;
+        case SurakartaDirection::RIGHT:
+            os << "RIGHT";
+            break;
+    }
+    return os;
+}
